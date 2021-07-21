a53c55c21755846208225bbcfdd4e9536671b498
Emily <lchutian@163.com>
2021-07-21 16:28:56 +0800

    add cli testcases

---
+++ testcases/cli-test/javapackages-tools/oe_test_build-classpath/oe_test_build-classpath.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+
+# Copyright (c) 2021. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+# #############################################
+# @Author    :   liujuan
+# @Contact   :   lchutian@163.com
+# @Date      :   2020/10/26
+# @License   :   Mulan PSL v2
+# @Desc      :   verify the uasge of build-classpath and build-classpath-directory command
+# ############################################
+
+source "../common/common_javapackages-tools.sh"
+
+function pre_test() {
+    LOG_INFO "Start to prepare the test environment."
+    deploy_env
+    LOG_INFO "Finish preparing the test environment."
+}
+
+function run_test() {
+    LOG_INFO "Start to run test."
+    build-classpath db | grep -E "/usr/share/java/db.jar"
+    CHECK_RESULT $?
+    build-classpath junit | grep -E "/usr/share/java/junit.jar"
+    CHECK_RESULT $?
+    build-classpath easymock beust-jcommander | grep -E "/usr/share/java/easymock.jar|/usr/share/java/beust-jcommander.jar"
+    CHECK_RESULT $?
+
+    build-classpath-directory --help | grep "Usage:"
+    CHECK_RESULT $?
+    build-classpath-directory --version | grep "[0-9]"
+    CHECK_RESULT $?
+    build-classpath-directory /usr/share/java | tr ':' '\n' | grep "/usr/share/java"
+    CHECK_RESULT $?
+    build-classpath-directory /usr/lib | tr ':' '\n' | grep "/usr/lib"
+    CHECK_RESULT $?
+    LOG_INFO "End of the test."
+}
+
+function post_test() {
+    LOG_INFO "Start to restore the test environment."
+    clear_env
+    LOG_INFO "Finish restoring the test environment."
+}
+
+main $@
---
+++ testcases/cli-test/javapackages-tools/oe_test_javapackages-local/oe_test_javapackages-local.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+# Copyright (c) 2021. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+# #############################################
+# @Author    :   liujuan
+# @Contact   :   lchutian@163.com
+# @Date      :   2020/10/26
+# @License   :   Mulan PSL v2
+# @Desc      :   verify the uasge of abs2rel and xmvn-builddep command
+# ############################################
+
+source "$OET_PATH/libs/locallibs/common_lib.sh"
+
+function pre_test() {
+    LOG_INFO "Start to prepare the test environment."
+    DNF_INSTALL javapackages-local
+    LOG_INFO "Finish preparing the test environment."
+}
+
+function run_test() {
+    LOG_INFO "Start to run test."
+    abs2rel /1/2/3/a/b/c /1/2/3 | grep "a/b/c"
+    CHECK_RESULT $?
+    abs2rel foo/bar foo/baz | grep "../bar"
+    CHECK_RESULT $?
+
+    gradle-local build >build.log
+    xmvn-builddep --help | grep "Usage: /usr/bin/xmvn-builddep"
+    CHECK_RESULT $?
+    xmvn-builddep build.log
+    CHECK_RESULT $?
+    LOG_INFO "End of the test."
+}
+
+function post_test() {
+    LOG_INFO "Start to restore the test environment."
+    rm -rf build.log .gradle build
+    DNF_REMOVE
+    LOG_INFO "Finish restoring the test environment."
+}
+
+main $@
