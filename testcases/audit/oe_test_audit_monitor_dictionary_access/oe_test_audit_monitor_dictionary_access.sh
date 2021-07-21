27dcfe7de18102d645c71a5a729c028ccdedacf1
厉铠源 <likaiyuan7@huawei.com>
2021-07-21 07:48:55 +0000

    update

--- testcases/audit/oe_test_audit_monitor_dictionary_access/oe_test_audit_monitor_dictionary_access.sh
+++ testcases/audit/oe_test_audit_monitor_dictionary_access/oe_test_audit_monitor_dictionary_access.sh
@@ -22,8 +22,7 @@ source ${OET_PATH}/libs/locallibs/common_lib.sh
 function run_test()
 {
     LOG_INFO "Start to run test."
-    systemctl start auditd
-    CHECK_RESULT $? 0 0 "start failed"
+    service auditd restart
     auditctl -D
     CHECK_RESULT $? 0 0 "delete failed"
     auditctl -w /opt -p wa -k opt_changes
