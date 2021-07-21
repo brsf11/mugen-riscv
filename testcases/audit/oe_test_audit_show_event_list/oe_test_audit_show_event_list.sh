dbaeef4e1324e2e1418020eb20ab254c98489644
厉铠源 <likaiyuan7@huawei.com>
2021-07-21 07:58:20 +0000

    update testcases/audit/oe_test_audit_show_event_list/oe_test_audit_show_event_list.sh.

--- testcases/audit/oe_test_audit_show_event_list/oe_test_audit_show_event_list.sh
+++ testcases/audit/oe_test_audit_show_event_list/oe_test_audit_show_event_list.sh
@@ -22,8 +22,7 @@ source ${OET_PATH}/libs/locallibs/common_lib.sh
 function run_test()
 {
     LOG_INFO "Start to run test."
-    systemctl start auditd
-    CHECK_RESULT $? 0 0 "start failed"
+    service auditd restart
     aureport -e -ts today -te today
     CHECK_RESULT $? 0 0 "aureport failed"
     LOG_INFO "End to run test."
