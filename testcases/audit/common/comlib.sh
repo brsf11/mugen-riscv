d784508ad4a5ff3493f091d10350209e3f5eef9e
厉铠源 <likaiyuan7@huawei.com>
2021-07-21 02:55:28 +0000

    update testcases/audit/common/comlib.sh.

--- testcases/audit/common/comlib.sh
+++ testcases/audit/common/comlib.sh
@@ -1,4 +1,22 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+# Copyright (c) 2021. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+####################################
+#@Author    	:   Jevons
+#@Contact   	:   1557927445@qq.com
+#@Date      	:   2021-04-16 11:39:43
+#@License   	:   Mulan PSL v2
+#@Version   	:   1.0
+#@Desc      	:   collect overall user time statistics
+#####################################
+
 source ${OET_PATH}/libs/locallibs/common_lib.sh
 AUDIT_PATH=$(find /etc -name auditd.conf)
 
