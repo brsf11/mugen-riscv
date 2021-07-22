66d1a7a8d751bdefb5bff4ee29e1a52c411a17e9
wjf008 <1136232498@qq.com>
2021-07-22 14:38:39 +0800

    add cli-test about geos,iperf3,python-wheel,sqlite-jdbc

---
+++ testcases/cli-test/geos/oe_test_geos_commandAndbaseFun/geos_test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include<stdlib.h>
+#include "geos.h"
+using namespace std;
+int main(){
+cout <<"geos's version is "<< GEOS_VERSION << endl;
+cout <<"geos's jts_port is "<< GEOS_JTS_PORT << endl;
+return 0;
+}
