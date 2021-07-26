fb16a94c6a41cf3934eae93359e19eb798b2e9b3
wjf008 <1136232498@qq.com>
2021-07-26 09:01:12 +0000

    删除文件 testcases/cli-test/open-isns

--- testcases/cli-test/open-isns/common/disk_info.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-disk_test=$(lsblk -r --output NAME,MOUNTPOINT | awk -F '/' '/sd|vd/ { dsk=substr($1,1,3);dsks[dsk]+=1 } END { for ( i in dsks ) { if (dsks[i]==1) print i } }'|tr -d "\r"|awk 'NR==1')
-echo -e "n\np\n1\n\n+2G\np\nw\n" | fdisk "/dev/${disk_test}"
-echo "${disk_test}"
