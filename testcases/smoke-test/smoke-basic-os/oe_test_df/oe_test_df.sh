1b2ce2c1695a4f77bcc296a757ee219f098169c9
wangpeng <wangpengb@uniontech.com>
2021-07-29 10:38:07 +0800

    add oe_test_df

---
+++ testcases/smoke-test/smoke-basic-os/oe_test_df/oe_test_df.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+  
+# Copyright (c) 2021 Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+# #############################################
+# @Author    :   wangpeng
+# @Contact   :   wangpengb@uniontech.com
+# @Date      :   2121-07-29
+# @License   :   Mulan PSL v2
+# @Desc      :   File system common command test-df
+# ############################################
+
+source "$OET_PATH/libs/locallibs/common_lib.sh"
+
+function pre_test() {
+    LOG_INFO "Start environment preparation."
+    LOG_INFO "End of environmental preparation!"
+}
+
+function run_test() {
+    LOG_INFO "Start testing..."
+    df | grep '/dev/mapper/openeuler-root'
+    CHECK_RESULT $? 0
+    df | grep 'G'
+    CHECK_RESULT $? 1
+    df -h | grep 'G'
+    CHECK_RESULT $? 0
+    df --help
+    CHECK_RESULT $? 0
+    LOG_INFO "Finish test!"
+}
+
+function post_test() {
+    LOG_INFO "start environment cleanup."
+    LOG_INFO "Finish environment cleanup!"
+}
+
+main $@
+
