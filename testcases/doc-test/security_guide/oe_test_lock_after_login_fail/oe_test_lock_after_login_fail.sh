4885ccd6fbea42f79a48c42201c72dd5105f65a1
yanglijin <yang_lijin@qq.com>
2021-07-30 01:31:11 +0000

    format oe_test_lock_after_login_fail.sh

--- testcases/doc-test/security_guide/oe_test_lock_after_login_fail/oe_test_lock_after_login_fail.sh
+++ testcases/doc-test/security_guide/oe_test_lock_after_login_fail/oe_test_lock_after_login_fail.sh
@@ -7,7 +7,7 @@
 # THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 # EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-# See the Mulan PSL v2 for more details.
+# See the Mulan PSL v2 for more detailAs.
 
 # #############################################
 # @Author    :   huyahui
@@ -19,41 +19,41 @@
 
 source "$OET_PATH/libs/locallibs/common_lib.sh"
 function pre_test() {
-	LOG_INFO "Start environmental preparation."
-	grep "^test:" /etc/passwd && userdel -rf test
-	ls testlog && rm -rf testlog
-	LOG_INFO "End of environmental preparation!"
+    LOG_INFO "Start environmental preparation."
+    grep "^test:" /etc/passwd && userdel -rf test
+    ls testlog && rm -rf testlog
+    LOG_INFO "End of environmental preparation!"
 }
 
 function run_test() {
-	LOG_INFO "Start testing..."
-	useradd test
-	passwd test <<EOF
+    LOG_INFO "Start testing..."
+    useradd test
+    passwd test <<EOF
 ${NODE1_PASSWORD}
 ${NODE1_PASSWORD}
 EOF
-	expect <<EOF1
-	log_file testlog
-	set timeout 15
-	spawn ssh test@127.0.0.1 
-	expect {
-		"*yes/no*" {
-		send "yes\\r"
-		}
-	}
-	expect {
-		"assword:" {
-		send "test\\r";
-                exp_continue;
-		}
-	
+    expect <<EOF1
+        log_file testlog
+        set timeout 15
+        spawn ssh test@127.0.0.1
+        expect {
+                "*yes/no*" {
+                send "yes\\r"
+                }
         }
-	expect eof
+        expect {
+                "assword:" {
+                send "test\\r";
+                exp_continue;
+                }
+
+        }
+        expect eof
 EOF1
-	[ $(grep -c 'Permission denied' testlog) -eq 3 ]
-	CHECK_RESULT $? 0 0 "grep 'Permission denied' failed"
-	rm -rf testlog
-        expect <<EOF1
+    [ $(grep -c 'Permission denied' testlog) -eq 3 ]
+    CHECK_RESULT $? 0 0 "grep 'Permission denied' failed"
+    rm -rf testlog
+    expect <<EOF1
         log_file testlog
         set timeout 15
         spawn ssh test@127.0.0.1
@@ -75,41 +75,42 @@ EOF1
         }
         expect eof
 EOF1
-        [ $(grep -c 'Permission denied' testlog) -eq 3 ]
-        CHECK_RESULT $? 0 0 "lock failed"
-	SLEEP_WAIT 45
-	expect <<EOF1
-	log_file testlog
-	set timeout 15
-	spawn ssh test@127.0.0.1 
-	expect {
-		"*yes/no*" {
-		send "yes\\r"
-		}
-	}
-	expect {
-		"assword:" {
-		send "${NODE1_PASSWORD}\\r"
-		}
-	}
-	expect {
-		"]" {
-		send "exit\\r"
-		}
-	}
-	expect eof
+    [ $(grep -c 'Permission denied' testlog) -eq 3 ]
+    CHECK_RESULT $? 0 0 "lock failed"
+    SLEEP_WAIT 45
+    expect <<EOF1
+        log_file testlog
+        set timeout 15
+        spawn ssh test@127.0.0.1
+        expect {
+                "*yes/no*" {
+                send "yes\\r"
+                }
+        }
+        expect {
+                "assword:" {
+                send "${NODE1_PASSWORD}\\r"
+                }
+        }
+        expect {
+                "]" {
+                send "exit\\r"
+                }
+        }
+        expect eof
 EOF1
-	grep '\[test@localhost' testlog
-	CHECK_RESULT $? 0 0 "login failed"
-	LOG_INFO "Finish test!"
+    grep '\[test@localhost' testlog
+    CHECK_RESULT $? 0 0 "login failed"
+    LOG_INFO "Finish test!"
 }
 
 function post_test() {
-	LOG_INFO "Start cleanning environment"
-	userdel -rf test
-	rm -rf testlog /run/faillock/test
-	SLEEP_WAIT 10
-	LOG_INFO "Finish environment cleanupp"
+    LOG_INFO "Start cleanning environment"
+    userdel -rf test
+    rm -rf testlog /run/faillock/test
+    SLEEP_WAIT 10
+    LOG_INFO "Finish environment cleanupp"
 }
 
 main "$@"
+
