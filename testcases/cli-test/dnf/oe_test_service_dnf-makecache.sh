2a91eada1a852fbaf7e3d78d9fa8813f39e978d6
huangrong <1820463064@qq.com>
2021-08-11 15:35:05 +0800

    update cli-test

--- testcases/cli-test/dnf/oe_test_service_dnf-makecache.sh
+++ testcases/cli-test/dnf/oe_test_service_dnf-makecache.sh
@@ -19,9 +19,20 @@
 
 source "../common/common_lib.sh"
 
+function pre_test() {
+    LOG_INFO "Start environmental preparation."
+    service=dnf-makecache.service
+    status='inactive (dead)'
+    LOG_INFO "End of environmental preparation!"
+}
+
 function run_test() {
     LOG_INFO "Start testing..."
-    test_oneshot dnf-makecache.service 'inactive (dead)'
+    systemctl status "${service}" | grep "Active" | grep -v "${status}"
+    CHECK_RESULT $? 0 1 "There is an error for the status of ${service}"
+    test_enabled "${service}"
+    journalctl -u "${service}" | grep -i "fail\|error" | grep -v -i "DEBUG\|INFO\|WARNING" | grep -v "Failed determining last makecache time"
+    CHECK_RESULT $? 0 1 "There is an error message for the log of ${service}"
     LOG_INFO "Finish test!"
 }
 
--- testcases/cli-test/dracut/oe_test_service_dracut-initqueue.sh
+++ testcases/cli-test/dracut/oe_test_service_dracut-initqueue.sh
@@ -30,28 +30,7 @@ function run_test() {
     LOG_INFO "Start testing..."
     systemctl status "${service}" | grep "Active" | grep -v "${status}"
     CHECK_RESULT $? 0 1 "There is an error for the status of ${service}"
-    state=$(systemctl is-enabled "${service}")
-    if [ "${state}" == "enabled" ]; then
-        symlink_file=$(systemctl disable "${service}" 2>&1 | awk '{print $2}' | awk '{print substr($0,1,length($0)-1)}')
-        find ${symlink_file}
-        CHECK_RESULT $? 0 1 "${service} disable failed"
-        systemctl enable "${service}"
-        find ${symlink_file}
-        CHECK_RESULT $? 0 0 "${service} enable failed"
-    elif [ "${state}" == "disable" ]; then
-        symlink_file=$(systemctl enable "${service}" 2>&1 | awk '{print $3}')
-        find ${symlink_file}
-        CHECK_RESULT $? 0 0 "${service} enable failed"
-        systemctl disable "${service}"
-        find ${symlink_file}
-        CHECK_RESULT $? 0 1 "${service} disable failed"
-    elif [ "${state}" == "masked" ]; then
-        LOG_INFO "Unit is masked, ignoring."
-    elif [ "${state}" == "static" ]; then
-        LOG_INFO "The unit files have no installation config. This means they are not meant to be enabled using systemctl."
-    else
-        LOG_INFO "Unit is indirect, ignoring."
-    fi
+    test_enabled "${service}"
     journalctl -u "${service}" | grep -i "fail\|error" | grep -v "ignorelockingfailure"
     CHECK_RESULT $? 0 1 "There is an error message for the log of ${service}"
     LOG_INFO "Finish test!"
