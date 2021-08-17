d7b213b475b4a7ca0e7ade841316f4f028e53a31
lemon-higgins <lemon.higgins@aliyun.com>
2021-08-17 10:31:16 +0800

    Update

--- libs/results_listener.sh
+++ libs/results_listener.sh
@@ -17,93 +17,62 @@
 #####################################
 
 SCRIPT_PATH=$(
-    cd "$(dirname "$0")" || exit 1
-    pwd
+	cd "$(dirname "$0")" || exit 1
+	pwd
 )
-MUGEN_PATH=$(dirname $SCRIPT_PATH)
 
-INTERVAL=10
+INTERVAL=2
 SUCCEED=0
 FAIL=0
 
-function usage() {
-    printf "Usage:  \n
-    This is a listener to achieve and post latest statistic of results
-    \n
-    -t:		configuration the number of intervala seconds of posting messages\n
-    -p:		configuration the URL of the server for posting messages\n
-    -h:		help
-    \n
-    Example: 
-        post the latest results every second to 127.0.0.1/test:
-          bash results_lisener.sh -t 1 -p 127.0.0.1/test
-        
-	post the latest results every hour to 127.0.0.1/test:
-	  bash results_lisener.sh -t 3600 -p 127.0.0.1/test
-        \n
-"
+function is_mugen_running() {
+	if ps -ef | grep -e 'mugen.sh' | grep -qv 'grep'; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function rsync_logs() {
+	[[ -z $(rpm -qa | grep rsync) ]] && yum install -y rsync
+	echo "$PASSWORD" >/tmp/rsync.password
+	chmod 600 /tmp/rsync.password
+	rsync -az --port=873 "${SCRIPT_PATH}"/../logs/* "${DEST_USER}"@"${DEST_IP}"::"${DEST_MODULE}"/"${JOB_NAME}" --password-file=/tmp/rsync.password
+	return 0
+}
+
+function check_results() {
+	if [[ -d ${SCRIPT_PATH}/../results/succeed ]]; then
+		SUCCEED=$(ls -l "${SCRIPT_PATH}/../results/succeed" | grep '^-' | wc -l)
+	else
+		SUCCEED=0
+	fi
+	if [[ -d ${SCRIPT_PATH}/../results/failed ]]; then
+		FAIL=$(ls -l "${SCRIPT_PATH}/../results/failed" | grep '^-' | wc -l)
+	else
+		FAIL=0
+	fi
 }
 
 function run_listener() {
-    while :
-    do
-        if [[ -d ${MUGEN_PATH}/results ]];then	
-	    if [[ -d ${MUGEN_PATH}/results/succeed ]];then
-	        SUCCEED=$(ls -l "${MUGEN_PATH}/results/succeed" | grep '^-' | wc -l)
-	    else
-		SUCCEED=0
-	    fi
-	    if [[ -d ${MUGEN_PATH}/results/failed ]];then
-	        FAIL=$(ls -l "${MUGEN_PATH}/results/failed" | grep '^-' | wc -l)
-	    else
-		FAIL=0
-	    fi
-        else
-	    SUCCEED=0
-	    FAIL=0
-        fi
-	
-        curl -d "{'succeed': $SUCCEED, 'fail': $FAID}" -H 'Content-Type: application/json'  -X POST $@ 
-	
-	sleep ${INTERVAL}s
+	while :; do
+		if [[ -d ${SCRIPT_PATH}/../results ]]; then
+			check_results
+		else
+			SUCCEED=0
+			FAIL=0
+		fi
 
-    done
+		curl -d "{'job_name': $JOB_NAME, 'succeed': $SUCCEED, 'fail': $FAIL}" -H 'Content-Type: application/json' -X POST "${SERVER_URL}"
+
+		rsync_logs
+
+		sleep ${INTERVAL}s
+
+		if ! is_mugen_running; then
+			check_results
+			curl -d "{'succeed': $SUCCEED, 'fail': $FAIL}" -H 'Content-Type: application/json' -X POST "${SERVER_URL}"
+			break
+		fi
+	done
 }
-
-while getopts "t:p:h" option;do
-	case $option in
-	t)
-	    if echo "$@" | grep -qe ' *-p *';then
-	        INTERVAL=$OPTARG
-	    else
-	        usage
-                exit 1
-            fi		
-	    ;;
-	p)
-		[[ ! -z $(echo "$@" | grep -e ' *-h *') ]] && {
-	        usage
-	        exit 1
-	    }
-	    if echo "$@" | grep -qe '^ *-p *.* *-t *';then
-		INTERVAL=$(echo "$@" | awk {'print $NF'})
-		[[ -z $(echo $INTERVAL | grep -e '^[[:digit:]]*$') ]] && {
-                    usage
-		    exit 1
-		}
-	    fi
-	    url=$OPTARG
-	    run_listener $url
-            ;;
-        h)
-	    usage
-	    exit 1
-	    ;;
-        *)
-	    usage
-	    exit 1
-	    ;;
-	esac
-done
-
-
