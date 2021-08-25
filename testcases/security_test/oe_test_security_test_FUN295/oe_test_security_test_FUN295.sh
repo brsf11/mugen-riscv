42e0e1522067f4eb2ab892568526f3a50bde68f1
厉铠源 <likaiyuan7@huawei.com>
2021-08-25 02:59:02 +0000

    update

--- testcases/security_test/oe_test_security_test_FUN295/oe_test_security_test_FUN295.sh
+++ testcases/security_test/oe_test_security_test_FUN295/oe_test_security_test_FUN295.sh
@@ -36,7 +36,7 @@ function run_test()
     CHECK_RESULT $? 0 0 "add root failed"
     useradd test 2>&1 | grep -e "useradd: user 'test' already exists"
     CHECK_RESULT $? 0 0 "add user failed"
-    CHECK_RESULT cat /etc/passwd | awk -F ":" '{a[$3]++}END{for(i in a){if(a[i]!=1){print i,a[i]}}}' 0 0 "XXXX"
+    CHECK_RESULT `cat /etc/passwd | awk -F ":" '{a[$3]++}END{for(i in a){if(a[i]!=1){print i,a[i]}}}' | wc -l` 0 0 "XXXX" 0 0 "print failed"
 
     LOG_INFO "End to run test."
 }
