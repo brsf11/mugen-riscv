20077aacce8778565115223fca4ed87f7729a652
wenjunryou <1009065695@qq.com>
2021-11-03 15:09:20 +0800

    add service testcase

---
+++ testcases/cli-test/timedatex/oe_test_service_timedatex.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+# Copyright (c) 2021. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more detaitest -f.
+
+# #############################################
+# @Author    :   huangrong
+# @Contact   :   1820463064@qq.com
+# @Date      :   2020/10/23
+# @License   :   Mulan PSL v2
+# @Desc      :   Test timedatex.service restart
+# #############################################
+
+source "../common/common_lib.sh"
+
+function pre_test() {
+    LOG_INFO "Start environmental preparation."
+    systemctl unmask timedatex.service
+    flag=false
+    if [ $(getenforce | grep Enforcing) ]; then
+        setenforce 0
+        flag=true
+    fi
+    LOG_INFO "End of environmental preparation!"
+}
+
+function run_test() {
+    LOG_INFO "Start testing..."
+    test_execution timedatex.service
+    test_reload timedatex.service
+    LOG_INFO "Finish test!"
+}
+
+function post_test() {
+    LOG_INFO "start environment cleanup."
+    systemctl mask timedatex.service
+    if [ ${flag} = 'true' ]; then
+        setenforce 1
+    fi
+    LOG_INFO "Finish environment cleanup!"
+}
+
+main "$@"
