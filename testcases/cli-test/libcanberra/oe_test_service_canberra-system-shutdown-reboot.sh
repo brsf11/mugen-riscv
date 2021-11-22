4dacc2aa10197d2de4c07bedfdfc35d517df9d02
huangrong <1820463064@qq.com>
2021-11-22 17:51:09 +0800

    add cli-test

---
+++ testcases/cli-test/libcanberra/oe_test_service_canberra-system-shutdown-reboot.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+# Copyright (c) 2021. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more detaitest -f.
+
+# #############################################
+# @Author    :   huangrong
+# @Contact   :   1820463064@qq.com
+# @Date      :   2020/10/23
+# @License   :   Mulan PSL v2
+# @Desc      :   Test canberra-system-shutdown-reboot.service restart
+# #############################################
+
+source "../common/common_lib.sh"
+
+function pre_test() {
+    LOG_INFO "Start environmental preparation."
+    DNF_INSTALL libcanberra
+    file_isExist=0
+    test -f /usr/share/sounds/freedesktop/stereo/system-shutdown.oga && file_isExist=1
+    test ${file_isExist} -eq 0 && cp -raf /usr/share/sounds/freedesktop/stereo/bell.oga /usr/share/sounds/freedesktop/stereo/system-shutdown.oga
+    systemctl start canberra-system-shutdown-reboot.service
+    LOG_INFO "End of environmental preparation!"
+}
+
+function run_test() {
+    LOG_INFO "Start testing..."
+    test_oneshot canberra-system-shutdown-reboot.service 'inactive (dead)'
+    LOG_INFO "Finish test!"
+}
+
+function post_test() {
+    LOG_INFO "start environment cleanup."
+    test ${file_isExist} -eq 0 && rm -rf /usr/share/sounds/freedesktop/stereo/system-shutdown.oga
+    DNF_REMOVE
+    LOG_INFO "Finish environment cleanup!"
+}
+
+main "$@"
