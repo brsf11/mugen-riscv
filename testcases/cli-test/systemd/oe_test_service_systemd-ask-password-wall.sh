89d63ba46e9d9e4f63019c22c0a8ed9be9e044ee
wenjunryou <1009065695@qq.com>
2022-01-14 16:35:24 +0800

    update Copyright

--- testcases/cli-test/systemd/oe_test_service_systemd-ask-password-wall.sh
+++ testcases/cli-test/systemd/oe_test_service_systemd-ask-password-wall.sh
@@ -27,7 +27,7 @@ function run_test() {
 }
 
 function post_test() {
-    LOG_INFO "start environment cleanup."
+    LOG_INFO "Start environment cleanup."
     systemctl stop systemd-ask-password-wall.service
     LOG_INFO "Finish environment cleanup!"
 }
