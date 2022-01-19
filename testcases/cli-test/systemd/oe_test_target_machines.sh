72baf376deb76798aa14a65996ad9b6ccb756eef
wenjunryou <1009065695@qq.com>
2022-01-19 16:42:17 +0800

    update systemd testcase

---
+++ testcases/cli-test/systemd/oe_test_target_machines.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+# Copyright (c) 2022. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+# #############################################
+# @Author    :   zengcongwei
+# @Contact   :   735811396@qq.com
+# @Date      :   2020/12/29
+# @License   :   Mulan PSL v2
+# @Desc      :   Test machine.target restart
+# #############################################
+
+source "../common/common_lib.sh"
+
+function run_test() {
+    LOG_INFO "Start to run test."
+    test_execution machines.target
+    test_reload machines.target
+    LOG_INFO "End of the test."
+}
+
+function post_test() {
+    LOG_INFO "Start environment cleanup."
+    systemctl stop machines.target
+    LOG_INFO "Finish environment cleanup!"
+}
+
+main "$@"
---
+++ testcases/cli-test/systemd/oe_test_target_remote-fs-pre.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+# Copyright (c) 2022. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+# #############################################
+# @Author    :   zengcongwei
+# @Contact   :   735811396@qq.com
+# @Date      :   2020/12/29
+# @License   :   Mulan PSL v2
+# @Desc      :   Test remote-fs-pre.target restart 
+# #############################################
+
+source "../common/common_lib.sh"
+
+function run_test() {
+    LOG_INFO "Start to run test."
+    systemctl restart remote-fs-pre.target 2>&1 | grep "it is configured to refuse manual start/stop"
+    CHECK_RESULT $? 0 0 "Check remote-fs-pre.target failed"
+    systemctl stop remote-fs-pre.target
+    CHECK_RESULT $? 0 0 "remote-fs-pre.target stop failed"
+    systemctl start remote-fs-pre.target 2>&1 | grep "it is configured to refuse manual start/stop"
+    CHECK_RESULT $? 0 0 "Check remote-fs-pre.target failed"
+    test_enabled remote-fs-pre.target
+    LOG_INFO "End of the test."
+}
+
+main "$@"
