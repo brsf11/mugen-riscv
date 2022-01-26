e3e28c674730050aec6a2dd6fea18612855f90c9
wenjunryou <1009065695@qq.com>
2022-01-26 17:58:10 +0800

    update

--- testcases/cli-test/corosync-qdevice/oe_test_service_corosync-qdevice.sh
+++ testcases/cli-test/corosync-qdevice/oe_test_service_corosync-qdevice.sh
@@ -24,12 +24,12 @@ function pre_test() {
     service=corosync-qdevice.service
     ha_pre
     DNF_INSTALL "corosync-qdevice corosync-qnetd"
-    P_SSH_CMD --node 2 --cmd "dnf install -y "dnf install -y corosync-qdevice corosync-qnetd"
+    P_SSH_CMD --node 2 --cmd "dnf install -y corosync-qdevice corosync-qnetd"
     P_SSH_CMD --node 3 --cmd "mv /etc/hosts /etc/hosts_bak"
     echo "${NODE3_IPV4} qdevice" >> /etc/hosts    
     SSH_SCP /etc/hosts ${NODE2_USER}@${NODE2_IPV4}:/etc/ "${NODE2_PASSWORD}"
     SSH_SCP /etc/hosts ${NODE3_USER}@${NODE3_IPV4}:/etc/ "${NODE3_PASSWORD}"
-    P_SSH_CMD --node 3 --cmd "dnf install -y corosync pacemaker pcs corosync-qdevice;
+    P_SSH_CMD --node 3 --cmd "dnf install -y corosync pacemaker pcs corosync-qdevice corosync-qnetd;
     systemctl start pcsd;
     hostnamectl set-hostname qdevice;
     systemctl stop firewalld;
@@ -57,10 +57,10 @@ function post_test() {
     hostnamectl set-hostname ${hostname};
     systemctl start firewalld;
     systemctl enable firewalld;
-    dnf remove -y corosync pacemaker pcs corosync-qdevice;
+    dnf remove -y corosync pacemaker pcs corosync-qdevice corosync-qnetd;
     mv /etc/hosts_bak /etc/hosts"
     DNF_REMOVE
-    P_SSH_CMD --node 2 --cmd "dnf install -y "dnf remove -y corosync-qdevice corosync-qnetd"
+    P_SSH_CMD --node 2 --cmd "dnf remove -y corosync-qdevice corosync-qnetd"
     ha_post
     LOG_INFO "Finish environment cleanup!"
 }
