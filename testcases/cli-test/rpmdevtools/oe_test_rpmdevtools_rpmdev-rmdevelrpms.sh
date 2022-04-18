ac4dc899b18f529d277483526d4fbb28e9b3472f
caimingxiang <mingxiang@isrc.iscas.ac.cn>
2022-04-18 14:01:33 +0800

    test_rpmdevtools

--- testcases/cli-test/rpmdevtools/oe_test_rpmdevtools_rpmdev-rmdevelrpms.sh
+++ testcases/cli-test/rpmdevtools/oe_test_rpmdevtools_rpmdev-rmdevelrpms.sh
@@ -21,67 +21,67 @@ source ${OET_PATH}/libs/locallibs/common_lib.sh
 
 
 function pre_test(){
-	LOG_INFO "Start environmental preparation."
-	DNF_INSTALL "rpmdevtools"
-	pkg_name=$(dnf list | head -n 3 | tail -n 1 | awk '{print $1}')
-	yumdownloader ${pkg_name}
-	test -d ~/rpmbuild && rm -rf ~/rpmbuild && LOG_INFO "Successfully deleted this dir."
-	LOG_INFO "End of environmental preparation."
+    LOG_INFO "Start environmental preparation."
+    DNF_INSTALL "rpmdevtools"
+    pkg_name=$(dnf list | head -n 3 | tail -n 1 | awk '{print $1}')
+    yumdownloader ${pkg_name}
+    test -d ~/rpmbuild && rm -rf ~/rpmbuild && LOG_INFO "Successfully deleted this dir."
+    LOG_INFO "End of environmental preparation."
 }
 
 function run_test(){
-	LOG_INFO "Start testing"
+    LOG_INFO "Start testing"
 
-	rpmdev-rmdevelrpms -h 
-	CHECK_RESULT $? 0 0 "Failed option: -h"
-	rpmdev-rmdevelrpms -v | grep 'version'
-	CHECK_RESULT $? 0 0 "Failed option: -v"
-	rpmdev-rmdevelrpms -l | grep -E 'devel|debuginfo|sdk|static|perl'
-	CHECK_RESULT $? 0 0 "Fail option: -l"
-	rpmdev-rmdevelrpms --qf test | grep 'Not removed due to dependencies'
-	CHECK_RESULT $? 0 0 "Fail option: --qf"
-	rpmdev-rmdevelrpms -y | grep 'Not removed due to dependencies'
-	CHECK_RESULT $? 0 0 "Fail option: -y"
+    rpmdev-rmdevelrpms -h 
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    rpmdev-rmdevelrpms -v | grep 'version'
+    CHECK_RESULT $? 0 0 "Failed option: -v"
+    rpmdev-rmdevelrpms -l | grep -E 'devel|debuginfo|sdk|static|perl'
+    CHECK_RESULT $? 0 0 "Fail option: -l"
+    rpmdev-rmdevelrpms --qf test | grep 'Not removed due to dependencies'
+    CHECK_RESULT $? 0 0 "Fail option: --qf"
+    rpmdev-rmdevelrpms -y | grep 'Not removed due to dependencies'
+    CHECK_RESULT $? 0 0 "Fail option: -y"
 
-	rpmdev-setuptree
-	test -d ~/rpmbuild
-	CHECK_RESULT $? 0 0 "Failed command: rpmdev-setuptree"
+    rpmdev-setuptree
+    test -d ~/rpmbuild
+    CHECK_RESULT $? 0 0 "Failed command: rpmdev-setuptree"
 
-	rpmdev-sha1 *rpm
-	CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha1"
+    rpmdev-sha1 *rpm
+    CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha1"
 
-	rpmdev-sha224 *rpm
-	CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha224"
+    rpmdev-sha224 *rpm
+    CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha224"
 
-	rpmdev-sha256 *rpm
-	CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha256"
+    rpmdev-sha256 *rpm
+    CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha256"
 
-	rpmdev-sha384 *rpm
-	CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha384"
+    rpmdev-sha384 *rpm
+    CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha384"
 
-	rpmdev-sha512 *rpm
-	CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha512"
+    rpmdev-sha512 *rpm
+    CHECK_RESULT $? 0 0 "Failed command: rpmdev-sha512"
 
-	var1="$(ls *rpm | rpmdev-sort | wc -l)"
-	[ "$var1" == 1 ]
-	CHECK_RESULT $? 0 0 "Failed command: rpmdev-sort"
-	rpmdev-sort -h
-	CHECK_RESULT $? 0 0 "Failed option: rpmdev-sort -h"
+    var1="$(ls *rpm | rpmdev-sort | wc -l)"
+    [ "$var1" == 1 ]
+    CHECK_RESULT $? 0 0 "Failed command: rpmdev-sort"
+    rpmdev-sort -h
+    CHECK_RESULT $? 0 0 "Failed option: rpmdev-sort -h"
 
-	rpmdev-sum *rpm
-	CHECK_RESULT $? 0 0 "Failed command: rpmdev-sum"
+    rpmdev-sum *rpm
+    CHECK_RESULT $? 0 0 "Failed command: rpmdev-sum"
 	
 
-	LOG_INFO "End to run test."
+    LOG_INFO "End to run test."
 
 }
 
 function post_test(){
-	LOG_INFO "Start to restore the test environment."
-	DNF_REMOVE
-	rm -rf ~/rpmbuild
-	rm *rpm
-	LOG_INFO "End to restore the test environment."
+    LOG_INFO "Start to restore the test environment."
+    DNF_REMOVE
+    rm -rf ~/rpmbuild
+    rm *rpm
+    LOG_INFO "End to restore the test environment."
 }
 
 main "$@"
--- testcases/cli-test/rpmdevtools/oe_test_rpmdevtools_rpmdev-vercmp.sh
+++ testcases/cli-test/rpmdevtools/oe_test_rpmdevtools_rpmdev-vercmp.sh
@@ -20,173 +20,173 @@
 source ${OET_PATH}/libs/locallibs/common_lib.sh
 
 function pre_test(){
-	LOG_INFO "Start environmental preparation."
-	DNF_INSTALL "rpmdevtools"
-	useradd user_test
-	su user_test -c "mkdir -p /home/user_test/rpmbuild/RPMS/x86_64"
-	pkg_name=$(dnf list | head -n 3 | tail -n 1 | awk '{print$1}')
-	yumdownloader ${pkg_name}
-	chown user_test *rpm
-	chmod 755 *rpm
-	mv *rpm /home/user_test/rpmbuild/RPMS/x86_64
+    LOG_INFO "Start environmental preparation."
+    DNF_INSTALL "rpmdevtools"
+    useradd user_test
+    su user_test -c "mkdir -p /home/user_test/rpmbuild/RPMS/x86_64"
+    pkg_name=$(dnf list | head -n 3 | tail -n 1 | awk '{print$1}')
+    yumdownloader ${pkg_name}
+    chown user_test *rpm
+    chmod 755 *rpm
+    mv *rpm /home/user_test/rpmbuild/RPMS/x86_64
 
-	yumdownloader ${pkg_name}
-	mkdir -p /ALT/Sisyphus/files/i586/RPMS
-	mkdir -p /ALT/Sisyphus/files/noarch/RPMS
-	mkdir -p /ALT/Sisyphus/files/SRPMS
-	wget https://repo.openeuler.org/openEuler-21.09/source/Packages/nodejsporter-1.0-2.oe1.src.rpm
-	mv *src.rpm /ALT/Sisyphus/files/SRPMS/
-	cp *rpm /ALT/Sisyphus/files/i586/RPMS/
-	cp *rpm /ALT/Sisyphus/files/noarch/RPMS
+    yumdownloader ${pkg_name}
+    mkdir -p /ALT/Sisyphus/files/i586/RPMS
+    mkdir -p /ALT/Sisyphus/files/noarch/RPMS
+    mkdir -p /ALT/Sisyphus/files/SRPMS
+    wget https://repo.openeuler.org/openEuler-21.09/source/Packages/nodejsporter-1.0-2.oe1.src.rpm
+    mv *src.rpm /ALT/Sisyphus/files/SRPMS/
+    cp *rpm /ALT/Sisyphus/files/i586/RPMS/
+    cp *rpm /ALT/Sisyphus/files/noarch/RPMS
 
-	DNF_INSTALL "${pkg_name}"
-	mkdir ./tmp_dir
+    DNF_INSTALL "${pkg_name}"
+    mkdir ./tmp_dir
 
-	wget https://gitee.com/src-openeuler/rpmdevtools/raw/master/rpmdevtools.spec
-	mkdir ./test_dir
-	rpmdev-setuptree
+    wget https://gitee.com/src-openeuler/rpmdevtools/raw/master/rpmdevtools.spec
+    mkdir ./test_dir
+    rpmdev-setuptree
 
-	LOG_INFO "End of environmental preparation."
+    LOG_INFO "End of environmental preparation."
 }
 
 
 function run_test(){
-	LOG_INFO "Start testing"
+    LOG_INFO "Start testing"
 
-	rpmdev-vercmp 1 1 2 2 1 2
-	CHECK_RESULT $? 12 0 "Failed option: n:n-n < n:n-n"
-	rpmdev-vercmp 2 1 2 1 1 2
-	CHECK_RESULT $? 11 0 "Failed option: n:n-n > n:n-n"
-	rpmdev-vercmp 2 1
-	CHECK_RESULT $? 11 0 "Failed option: n > n"
-	rpmdev-vercmp 1 2
-	CHECK_RESULT $? 12 0 "Failed option: n < n"
-	rpmdev-vercmp 1 1
-	CHECK_RESULT $? 0 0 "Failed option: n == n"
-	rpmdev-vercmp 2 1 2 2 1 2
-	CHECK_RESULT $? 0 0 "Failed option: n:n-n == n:n-n"
-	rpmdev-vercmp -h
-	CHECK_RESULT $? 0 0 "Failed option: -h"
+    rpmdev-vercmp 1 1 2 2 1 2
+    CHECK_RESULT $? 12 0 "Failed option: n:n-n < n:n-n"
+    rpmdev-vercmp 2 1 2 1 1 2
+    CHECK_RESULT $? 11 0 "Failed option: n:n-n > n:n-n"
+    rpmdev-vercmp 2 1
+    CHECK_RESULT $? 11 0 "Failed option: n > n"
+    rpmdev-vercmp 1 2
+    CHECK_RESULT $? 12 0 "Failed option: n < n"
+    rpmdev-vercmp 1 1
+    CHECK_RESULT $? 0 0 "Failed option: n == n"
+    rpmdev-vercmp 2 1 2 2 1 2
+    CHECK_RESULT $? 0 0 "Failed option: n:n-n == n:n-n"
+    rpmdev-vercmp -h
+    CHECK_RESULT $? 0 0 "Failed option: -h"
 
-	test -e /home/user_test/rpmbuild/RPMS/x86_64/*rpm 
-	CHECK_RESULT $? 0 0 "Failed pre_test"
-	su user_test -c "rpmdev-wipetree"
-	test -e /home/user_test/rpmbuild/RPMS/x86_64/*rpm
-	CHECK_RESULT $? 1 0 "Failed command: rpmdev-wipetree"
+    test -e /home/user_test/rpmbuild/RPMS/x86_64/*rpm 
+    CHECK_RESULT $? 0 0 "Failed pre_test"
+    su user_test -c "rpmdev-wipetree"
+    test -e /home/user_test/rpmbuild/RPMS/x86_64/*rpm
+    CHECK_RESULT $? 1 0 "Failed command: rpmdev-wipetree"
 
-	rpmelfsym -p *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -p"
-	rpmelfsym -h | grep 'Options:'
-	CHECK_RESULT $? 0 0 "Failed option: -h"
-	rpmelfsym -a
-	CHECK_RESULT $? 0 0 "Failed option: -a"
+    rpmelfsym -p *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -p"
+    rpmelfsym -h | grep 'Options:'
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    rpmelfsym -a
+    CHECK_RESULT $? 0 0 "Failed option: -a"
 
-	rpmfile -p *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -p"
-	rpmfile -h | grep 'Options:'
-	CHECK_RESULT $? 0 0 "Failed option: -h"
-	rpmfile -a
-	CHECK_RESULT $? 0 0 "Failed option: -a"
+    rpmfile -p *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -p"
+    rpmfile -h | grep 'Options:'
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    rpmfile -a
+    CHECK_RESULT $? 0 0 "Failed option: -a"
 
-	rpminfo -h | grep 'Usage'
-	CHECK_RESULT $? 0 0 "Failed option: -h"
-	rpminfo -v *rpm 
-	CHECK_RESULT $? 0 0 "Failed option: -v"
-	rpminfo -q *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -q"
-	rpminfo -qq *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -qq"
-	rpminfo -i -o record
-	test -e record
-	CHECK_RESULT $? 0 0 'Failed option: -i -o'
-	rpminfo -e *rpm 
-	CHECK_RESULT $? 0 0 "Failed option: -e"
-	rpminfo -l *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -l"
-	rpminfo -p *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -p"
-	rpminfo -np *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -np"
-	rpminfo -nP *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -nP"
-	rpminfo -r *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -r"
-	rpminfo -ro *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -ro"
-	rpminfo -s -o record1 *rpm
-	test -e record1*
-	CHECK_RESULT $? 0 0 "Failed option: -s "
-	rpminfo -t *rpm
-	CHECK_RESULT $? 0 0 "Failed option: -t"
-	rpminfo -T ./tmp_dir *rpm
+    rpminfo -h | grep 'Usage'
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    rpminfo -v *rpm 
+    CHECK_RESULT $? 0 0 "Failed option: -v"
+    rpminfo -q *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -q"
+    rpminfo -qq *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -qq"
+    rpminfo -i -o record
+    test -e record
+    CHECK_RESULT $? 0 0 'Failed option: -i -o'
+    rpminfo -e *rpm 
+    CHECK_RESULT $? 0 0 "Failed option: -e"
+    rpminfo -l *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -l"
+    rpminfo -p *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -p"
+    rpminfo -np *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -np"
+    rpminfo -nP *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -nP"
+    rpminfo -r *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -r"
+    rpminfo -ro *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -ro"
+    rpminfo -s -o record1 *rpm
+    test -e record1*
+    CHECK_RESULT $? 0 0 "Failed option: -s "
+    rpminfo -t *rpm
+    CHECK_RESULT $? 0 0 "Failed option: -t"
+    rpminfo -T ./tmp_dir *rpm
 
-	rpmls -l *rpm
+    rpmls -l *rpm
 
-	rpmpeek -h | grep "Options:"
-	CHECK_RESULT $? 0 0 "Failed option: -h"
-	rpmpeek *rpm ls -l | grep "usr"
-	CHECK_RESULT $? 0 0 "Failed command: rpmpeek"
-	rpmpeek -n attr*rpm ls -l 
-	CHECK_RESULT $? 0 0 "Failed option: -n"
+    rpmpeek -h | grep "Options:"
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    rpmpeek *rpm ls -l | grep "usr"
+    CHECK_RESULT $? 0 0 "Failed command: rpmpeek"
+    rpmpeek -n attr*rpm ls -l 
+    CHECK_RESULT $? 0 0 "Failed option: -n"
 
-	rpmsodiff -h | grep "Usage:"
-	CHECK_RESULT $? 0 0 "Failed option: -h"
-	rpmsodiff *rpm *rpm
-	CHECK_RESULT $? 0 0 "Failed command: rpmsodiff"
+    rpmsodiff -h | grep "Usage:"	
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    rpmsodiff *rpm *rpm
+    CHECK_RESULT $? 0 0 "Failed command: rpmsodiff"
 
-	rpmsoname -h | grep "Options:"
-	CHECK_RESULT $? 0 0 "Failed option: -h"
-	rpmsoname -p *rpm | grep "/usr/lib64"
-	CHECK_RESULT $? 0 0 "Failed option: -p"
-	rpmsoname . | grep "/usr/lib64"
-	CHECK_RESULT $? 0 0 "Failed command: rpmsoname"
+    rpmsoname -h | grep "Options:"
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    rpmsoname -p *rpm | grep "/usr/lib64"
+    CHECK_RESULT $? 0 0 "Failed option: -p"
+    rpmsoname . | grep "/usr/lib64"
+    CHECK_RESULT $? 0 0 "Failed command: rpmsoname"
 
-	spectool -l rpmdevtools.spec | grep "Source"
-	CHECK_RESULT $? 0 0 "Failed option: -l"
-	spectool -g rpmdevtools.spec && test -e *tar.xz
-	CHECK_RESULT $? 0 0 "Failed option: -g"
-	spectool -h | grep "Options"
-	CHECK_RESULT $? 0 0 "Failed option: -h"
-	spectool -A rpmdevtools.spec | grep "Source"
-	CHECK_RESULT $? 0 0 "Failed option: -A"
-	spectool -S rpmdevtools.spec | grep "Source"
-	CHECK_RESULT $? 0 0 "Failed option: -S"
-	spectool -P rpmdevtools.spec | grep "Patch"
-	CHECK_RESULT $? 0 0 "Failed option: -P"
-	spectool -s 0 rpmdevtools.spec | grep "Source0"
-	CHECK_RESULT $? 0 0 "Failed option: -s"
-	spectool -p 0 rpmdevtools.spec | grep "Patch0"
-	CHECK_RESULT $? 0 0 "Failed option: -p"
-	spectool -d 'test test1' rpmdevtools.spec
-	CHECK_RESULT $? 0 0 "Failed option: -d"
-	spectool -g -C ./test_dir rpmdevtools.spec && test -e ./test_dir/*tar.xz
-	CHECK_RESULT $? 0 0 "Failed option: -C"
-	spectool -g -R rpmdevtools.spec && test -e ~/rpmbuild/SOURCES/*tar.xz 
-	CHECK_RESULT $? 0 0 "Failed option: -R"
-	test -e *tar.xz && spectool -g -f rpmdevtools.spec
-	CHECK_RESULT $? 0 0 "Failed option: -f"
-	rm *tar.gz
-	spectool -g -n rpmdevtools.spec
-	CHECK_RESULT $? 0 0 "Failed option: -n"
-	spectool -D rpmdevtools.spec 
-	CHECK_RESULT $? 0 0 "Failed option: -D"
+    spectool -l rpmdevtools.spec | grep "Source"
+    CHECK_RESULT $? 0 0 "Failed option: -l"
+    spectool -g rpmdevtools.spec && test -e *tar.xz
+    CHECK_RESULT $? 0 0 "Failed option: -g"
+    spectool -h | grep "Options"
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    spectool -A rpmdevtools.spec | grep "Source"
+    CHECK_RESULT $? 0 0 "Failed option: -A"
+    spectool -S rpmdevtools.spec | grep "Source"
+    CHECK_RESULT $? 0 0 "Failed option: -S"
+    spectool -P rpmdevtools.spec | grep "Patch"
+    CHECK_RESULT $? 0 0 "Failed option: -P"
+    spectool -s 0 rpmdevtools.spec | grep "Source0"
+    CHECK_RESULT $? 0 0 "Failed option: -s"
+    spectool -p 0 rpmdevtools.spec | grep "Patch0"
+    CHECK_RESULT $? 0 0 "Failed option: -p"
+    spectool -d 'test test1' rpmdevtools.spec
+    CHECK_RESULT $? 0 0 "Failed option: -d"
+    spectool -g -C ./test_dir rpmdevtools.spec && test -e ./test_dir/*tar.xz
+    CHECK_RESULT $? 0 0 "Failed option: -C"
+    spectool -g -R rpmdevtools.spec && test -e ~/rpmbuild/SOURCES/*tar.xz 
+    CHECK_RESULT $? 0 0 "Failed option: -R"
+    test -e *tar.xz && spectool -g -f rpmdevtools.spec
+    CHECK_RESULT $? 0 0 "Failed option: -f"
+    rm *tar.gz
+    spectool -g -n rpmdevtools.spec
+    CHECK_RESULT $? 0 0 "Failed option: -n"
+    spectool -D rpmdevtools.spec 
+    CHECK_RESULT $? 0 0 "Failed option: -D"
 
-	LOG_INFO "End to run test."
+    LOG_INFO "End to run test."
 }
 
 function post_test(){
-	LOG_INFO "Start to restore the test environment."
-	DNF_REMOVE
-	userdel -r user_test
-	rm *rpm
-	rm -rf /ALT
-	rm record*
-	rm -r ./tmp_dir
-	rm -rf ./test_dir
-	rm -rf ~/rpmbuild
-	rm -rf ./rpmdevtools
-	rm rpmdevtools*
-	LOG_INFO "End to restore the test environment."
+    LOG_INFO "Start to restore the test environment."
+    DNF_REMOVE
+    userdel -r user_test
+    rm *rpm
+    rm -rf /ALT
+    rm record*
+    rm -r ./tmp_dir
+    rm -rf ./test_dir
+    rm -rf ~/rpmbuild
+    rm -rf ./rpmdevtools
+    rm rpmdevtools*
+    LOG_INFO "End to restore the test environment."
 }
 
 
