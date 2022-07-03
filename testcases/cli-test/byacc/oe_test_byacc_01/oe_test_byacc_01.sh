11441389126b15ea6114050623b520e219c08b11
binshuo zu <binshuo@isrc.iscas.ac.cn>
2022-07-04 06:03:59 +0800

    test: add testcase for package byacc

---
+++ testcases/cli-test/byacc/oe_test_byacc_01/oe_test_byacc_01.sh
@@ -0,0 +1,94 @@
+# Copyright (c) 2022. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+####################################
+# @Author    	:   zu binshuo
+# @Contact   	:   binshuo@isrc.iscas.ac.cn
+# @Date      	:   2022-7-15
+# @License   	:   Mulan PSL v2
+# @Desc      	:   the test of byacc package
+####################################
+
+source ${OET_PATH}/libs/locallibs/common_lib.sh
+
+function pre_test() {
+    LOG_INFO "Start environmental preparation."
+    DNF_INSTALL byacc
+    cp -rf ../common ./tmp
+    cd ./tmp
+    LOG_INFO "End of environmental preparation!"
+}
+
+function run_test() {
+    LOG_INFO "Start testing..."
+
+    byacc -V 2>&1 | grep "byacc - " | grep '[[:digit:]]*'
+    CHECK_RESULT $? 0 0 "Failed option: -v"
+    byacc -V 2>&1 | grep "byacc - " | grep '[[:digit:]]*'
+    CHECK_RESULT $? 0 0 "Failed option: -v"
+
+    byacc -h 2>&1 | grep 'Usage: byacc'
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    byacc --help 2>&1 | grep 'Usage: byacc'
+    CHECK_RESULT $? 0 0 "Failed option: --help"
+
+    byacc -b test_b test.y 
+    ls | grep "test_b.tab.c" 
+    CHECK_RESULT $? 0 0 "Failed option: -b"
+    byacc --file-prefix test_lb test.y 
+    ls | grep "test_lb.tab.c" 
+    CHECK_RESULT $? 0 0 "Failed option: --file-prefix"
+
+    byacc -H test_h.c test.y
+    ls | grep "test_h.c" 
+    CHECK_RESULT $? 0 0 "Failed option: -H"
+    byacc --defines test_lh.c test.y
+    ls | grep "test_lh.c" 
+    CHECK_RESULT $? 0 0 "Failed option: --defines"
+
+    byacc test.y -o test_o.tab.c
+    ls | grep "test_o.tab.c"
+    CHECK_RESULT $? 0 0 "Failed option: -o"
+    byacc test.y --output test_lo.tab.c
+    ls | grep "test_lo.tab.c"
+    CHECK_RESULT $? 0 0 "Failed option: --output"
+
+    byacc -g test.y -o test_g.dot
+    ls | grep "test_g.dot"
+    CHECK_RESULT $? 0 0 "Failed option: -g"
+    byacc --graph test.y -o test_lg.dot
+    ls | grep "test_lg.dot"
+    CHECK_RESULT $? 0 0 "Failed option: --graph"
+
+    byacc -p test_p test.y -o test_p.tab.c
+    cat test_p.tab.c | grep -m 1 "test_p"
+    CHECK_RESULT $? 0 0 "Failed option: -p"
+    byacc --name-prefix test_lp test.y -o test_lp.tab.c
+    cat test_lp.tab.c | grep -m 1 "test_lp"
+    CHECK_RESULT $? 0 0 "Failed option: --name-prefix"
+
+    byacc -l test.y -o test_l.tab.c
+    cat test_l.tab.c | grep "#line"
+    CHECK_RESULT $? 0 "Failed option: -l" 0 
+    byacc --no-lines test.y -o test_ll.tab.c
+    cat test_ll.tab.c | grep "#line"
+    CHECK_RESULT $? 0 "Failed option: --no-lines" 0
+
+
+    LOG_INFO "Finish test!"
+
+}
+
+function post_test() {
+    LOG_INFO "start environment cleanup."
+    DNF_REMOVE
+    cd .. && rm -rf ./tmp
+    LOG_INFO "Finish environment cleanup!"
+}
+
+main "$@"
---
+++ testcases/cli-test/byacc/oe_test_yacc_01/oe_test_yacc_01.sh
@@ -0,0 +1,92 @@
+# Copyright (c) 2022. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+####################################
+# @Author    	:   zu binshuo
+# @Contact   	:   binshuo@isrc.iscas.ac.cn
+# @Date      	:   2022-7-15
+# @License   	:   Mulan PSL v2
+# @Desc      	:   the test of byacc package
+####################################
+
+source ${OET_PATH}/libs/locallibs/common_lib.sh
+
+function pre_test() {
+    LOG_INFO "Start environmental preparation."
+    DNF_INSTALL byacc
+    cp -rf ../common ./tmp
+    cd ./tmp
+    LOG_INFO "End of environmental preparation!"
+}
+
+function run_test() {
+    LOG_INFO "Start testing..."
+
+    yacc -V 2>&1 | grep "yacc - " | grep '[[:digit:]]*'
+    CHECK_RESULT $? 0 0 "Failed option: -v"
+    yacc -V 2>&1 | grep "yacc - " | grep '[[:digit:]]*'
+    CHECK_RESULT $? 0 0 "Failed option: -v"
+
+    yacc -h 2>&1 | grep 'Usage: yacc'
+    CHECK_RESULT $? 0 0 "Failed option: -h"
+    yacc --help 2>&1 | grep 'Usage: yacc'
+    CHECK_RESULT $? 0 0 "Failed option: --help"
+
+    yacc -b test_b test.y 
+    ls | grep "test_b.tab.c" 
+    CHECK_RESULT $? 0 0 "Failed option: -b"
+    yacc --file-prefix test_lb test.y 
+    ls | grep "test_lb.tab.c" 
+    CHECK_RESULT $? 0 0 "Failed option: --file-prefix"
+
+    yacc -H test_h.c test.y
+    ls | grep "test_h.c" 
+    CHECK_RESULT $? 0 0 "Failed option: -H"
+    yacc --defines test_lh.c test.y
+    ls | grep "test_lh.c" 
+    CHECK_RESULT $? 0 0 "Failed option: --defines"
+
+    yacc test.y -o test_o.tab.c
+    ls | grep "test_o.tab.c"
+    CHECK_RESULT $? 0 0 "Failed option: -o"
+    yacc test.y --output test_lo.tab.c
+    ls | grep "test_lo.tab.c"
+    CHECK_RESULT $? 0 0 "Failed option: --output"
+
+    yacc -g test.y -o test_g.dot
+    ls | grep "test_g.dot"
+    CHECK_RESULT $? 0 0 "Failed option: -g"
+    yacc --graph test.y -o test_lg.dot
+    ls | grep "test_lg.dot"
+    CHECK_RESULT $? 0 0 "Failed option: --graph"
+
+    yacc -p test_p test.y -o test_p.tab.c
+    cat test_p.tab.c | grep -m 1 "test_p"
+    CHECK_RESULT $? 0 0 "Failed option: -p"
+    yacc --name-prefix test_lp test.y -o test_lp.tab.c
+    cat test_lp.tab.c | grep -m 1 "test_lp"
+    CHECK_RESULT $? 0 0 "Failed option: --name-prefix"
+
+    yacc -l test.y -o test_l.tab.c
+    cat test_l.tab.c | grep "#line"
+    CHECK_RESULT $? 0 "Failed option: -l" 0 
+    yacc --no-lines test.y -o test_ll.tab.c
+    cat test_ll.tab.c | grep "#line"
+    CHECK_RESULT $? 0 "Failed option: --no-lines" 0 
+
+    LOG_INFO "Finish test!"
+}
+
+function post_test() {
+    LOG_INFO "start environment cleanup."
+    DNF_REMOVE
+    cd .. && rm -rf ./tmp
+    LOG_INFO "Finish environment cleanup!"
+}
+
+main "$@"
